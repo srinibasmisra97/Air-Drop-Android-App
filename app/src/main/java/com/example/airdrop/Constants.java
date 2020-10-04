d1cd742f2ac92d338bf5672a279ce6052aa768b3
Srinibas Misra <srinibas.misra97@gmail.com>
2020-10-04 11:42:25 +0530

    updated channel title Constants

--- app/src/main/java/com/example/airdrop/Constants.java
+++ app/src/main/java/com/example/airdrop/Constants.java
@@ -2,7 +2,7 @@ package com.example.airdrop;
 
 public class Constants {
 
-    public static final String SERVER_HOST = "<Your Server URL>";
-    public static final String PULL_NOTIFICATION_CHANNEL_TITLE = "Air Drop Pull Service";
-    public static final String DOWNLOAD_NOTIFICATION_CHANNEL_TITLE = "";
+    public static final String SERVER_HOST = "https://air-drop.srinibasmisra.com";
+    public static final String PULL_NOTIFICATION_CHANNEL_TITLE = "Pull Service";
+    public static final String DOWNLOAD_NOTIFICATION_CHANNEL_TITLE = "Donwload Service";
 }
